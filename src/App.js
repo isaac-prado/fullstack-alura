848388620927a1db81e9357317c20cd08acc8758
Isaac <isaacpradoalmeida@gmail.com>
2024-02-19 12:25:00 -0300

    preparando o ambiente

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <div className="">
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
